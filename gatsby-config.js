bc79a2fd2b0dcf947ddec825f914691c72239602
aibunny <waihenyafredrick@gmail.com>
2025-01-10 10:49:06 +0300

    feat[about]: going bullish

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Bunny!',
-        short_name: 'Fredrick Waihenya',
+        short_name: 'Bunny',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
